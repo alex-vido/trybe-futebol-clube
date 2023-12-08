767303a51a2b0795dc010a6348d0091010983683
Alex Vido <alex.vido93@gmail.com>
2023-12-08 19:45:32 -0300

    req 4: test endpoint teams/:id

--- app/backend/src/tests/Team.test.ts
+++ app/backend/src/tests/Team.test.ts
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-import { Teams } from './mocks/Team.mock';
+import { Teams, Team } from './mocks/Team.mock';
 import TeamModel from '../database/models/teams.model';
 
 describe('Teams Test', function() {
@@ -26,4 +26,19 @@ describe('Teams Test', function() {
     expect(body).to.deep.equal(Teams);
 
   });
+
+  it('should return a Team by id', async function() {
+
+    const id = 1;
+
+    sinon.stub(TeamModel, 'findByPk').resolves(Team as any);
+
+    const { status, body } = await chai.request(app).get(`/teams/${id}`);
+
+    expect(status).to.equal(200);
+
+    expect(body).to.deep.equal(Team);
+
+  });
+
 });
