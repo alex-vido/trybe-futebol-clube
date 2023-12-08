d924193a8048a89eaf47b5d08ecab16e86c4c55b
Alex Vido <alex.vido93@gmail.com>
2023-12-08 19:14:26 -0300

    req 2: test Team endpoint

---
+++ app/backend/src/tests/Team.test.ts
@@ -0,0 +1,29 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+// import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+import { Teams } from './mocks/Team.mock';
+import TeamModel from '../database/models/teams.model';
+
+describe('Books Test', function() {
+
+  it('should return all books', async function() {
+
+    sinon.stub(TeamModel, 'findAll').resolves(Teams as any);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.equal(200);
+
+    expect(body).to.deep.equal(Teams);
+
+  });
+});
