c4c214fa7d89415e3211ba0ee57b88aed81cc156
Alex Vido <alex.vido93@gmail.com>
2023-12-08 18:30:30 -0300

    req 1: created Dockerfile, Teams migration and Model

---
+++ app/backend/src/database/migrations/1-create-teams.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { TeamInterface } from '../../Interfaces/TeamInterface';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<TeamInterface>> ('teams',{
+      id:{
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+      },
+      teamName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          field: 'team_name',
+      },
+    });
+  },
+
+  down(queryInterface: QueryInterface): void {
+    queryInterface.dropTable("teams");
+  }
+}
---
+++ app/backend/src/database/models/teams.model.ts
@@ -0,0 +1,34 @@
+import {
+  Model,
+  DataTypes,
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize';
+
+import db from '.';
+
+export default class TeamModel extends Model<InferAttributes<TeamModel>,
+InferCreationAttributes<TeamModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
